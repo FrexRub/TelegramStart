54ec9c3f7e69d63c32fc80a27d0c4eeef5e79b8d
Станислав Рубцов <frexrub@gmail.com>
2025-09-19 16:14:21 +0300

    commands: create set_commands

---
+++ src/core/commands.py
@@ -0,0 +1,12 @@
+from aiogram.types import BotCommand, BotCommandScopeDefault
+
+from src.core.config import bot
+
+
+async def set_commands():
+    commands = [
+        BotCommand(command="start", description="Старт"),
+        BotCommand(command="next", description="Спец. клавиатура"),
+        BotCommand(command="help", description="Опрос"),
+    ]
+    await bot.set_my_commands(commands, BotCommandScopeDefault())
--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ import logging
 
 from src.core.config import configure_logging, bot, dp
 from src.handlers.start import router as start_router
+from src.core.commands import set_commands
 
 configure_logging(logging.INFO)
 logger = logging.getLogger(__name__)
@@ -13,6 +14,7 @@ dp.include_router(start_router)
 async def main():
     logger.info("Start Bot")
     await dp.start_polling(bot)
+    await set_commands()
 
 
 if __name__ == "__main__":
