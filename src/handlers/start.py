d8433af2b199f3297396d7c545a3bdae2d1690d7
Станислав Рубцов <frexrub@gmail.com>
2025-09-23 14:33:01 +0300

    commands: command with args

--- src/handlers/start.py
+++ src/handlers/start.py
@@ -1,5 +1,5 @@
 from aiogram import Router, F
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart, Command, CommandObject
 from aiogram.types import Message
 
 from src.keyboards.all_kb import main_kb, create_space_kb, create_rat
@@ -8,10 +8,18 @@ router = Router()
 
 
 @router.message(CommandStart())
-async def cmd_start(message: Message):
-    await message.answer(
-        "Запуск сообщения по команде /start", reply_markup=main_kb(message.from_user.id)
-    )
+async def cmd_start(message: Message, command: CommandObject):
+    command_arg: str = command.args # https://t.me/your_bot?start=hi -> args="hi"
+    if command_arg:
+        await message.answer(
+            f"Запуск сообщения по команде /start with command_arg: {command_arg}",
+            reply_markup=main_kb(message.from_user.id),
+        )
+    else:
+        await message.answer(
+            "Запуск сообщения по команде /start without command_arg",
+            reply_markup=main_kb(message.from_user.id),
+        )
 
 
 @router.message(Command("next"))
